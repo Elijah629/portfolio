c0a0ee20e01c1df142d9c6d69018081fa225a109
Elijah629 <62805599+Elijah629@users.noreply.github.com>
2023-02-25 17:07:18 -0800

    fix: codemod built-in-next-font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { Cousine } from "@next/font/google";
+import { Cousine } from "next/font/google";
 import { Footer } from "@/components/interface/Footer";
 
 const cousine = Cousine({
